75e31f57156ea167ead534c80d581a07412a5561
thcrt <110127860+thcrt@users.noreply.github.com>
2024-06-29 23:58:13 +0200

    Add a record viewing page design

--- src/nerd/__init__.py
+++ src/nerd/__init__.py
@@ -6,5 +6,9 @@ def create_app() -> f.app:
     @app.route("/")
     def index() -> str:
         return f.render_template("index.jinja")
+    
+    @app.route("/record")
+    def record() -> str:
+        return f.render_template("record.jinja")
 
     return app
